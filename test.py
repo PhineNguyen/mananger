27be7b1cde8c0e24c27c615705cdbb526996cf71
TuanN05 <tuannt1436@ut.edu.vn>
2024-11-01 14:59:40 +0700

    bg canvas

--- test.py
+++ test.py
@@ -13,15 +13,21 @@ window.configure(background="#EAE7D6")
 
 #Thành phần giao diện 
 #bảng trên cùng
-canvas = tk.Canvas(window, bg="#B1C6B4", width=2000, height=200)
+canvas = tk.Canvas(window, width=2000, height=200)
+canvas.config(bg="#B1C6B4")
 canvas.pack()
+
 
 #Bảng ngoài bìa 
 canvas = tk.Canvas(window, bg="#B1C6B4",width=515,height= 835)
+canvas.config(bg="#B1C6B4")
+
 canvas.place(x=1400, y=205)
 
 #Bảng ở trung tâm
 canvas = tk.Canvas(window, bg="#B1C6B4",width=1390,height =835)
+canvas.config(bg="#B1C6B4")
+
 canvas.place(x=4,y=205)
 
 #tạo các hàng và cột cho bảng 
