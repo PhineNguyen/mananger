f94eaa1ac420d015d31420770a684a000399acae
TuanN05 <tuannt1436@ut.edu.vn>
2024-11-02 10:09:25 +0700

    san pham

--- main.py
+++ main.py
@@ -5,20 +5,24 @@ from don_hang import create_don_hang_tab
 from thong_ke import create_thong_ke_tab
 from khach_hang import create_khach_hang_tab
 
-# Initialize main application window
+# Khởi tạo cửa sổ chính
 app = ttk.Window(themename="minty")
 app.title("Quản Lý Cửa Hàng")
 app.geometry("800x500")
 
-# Create Notebook for tabs
-notebook = ttk.Notebook(app)
+# Tạo style cho tab
+style = ttk.Style()
+style.configure("TNotebook.Tab", padding=[100, 5], font=('Helvetica', 20))
+
+# Tạo notebook với style tùy chỉnh
+notebook = ttk.Notebook(app, style="TNotebook")  # Áp dụng style chuẩn TNotebook
 notebook.pack(fill=BOTH, expand=TRUE)
 
-# Add tabs to the notebook
+# Thêm các tab vào notebook
 create_san_pham_tab(notebook)
-create_don_hang_tab(notebook,app)
+create_don_hang_tab(notebook, app)
 create_thong_ke_tab(notebook)
 create_khach_hang_tab(notebook, app)
 
-# Start the application
+# Khởi chạy ứng dụng
 app.mainloop()
